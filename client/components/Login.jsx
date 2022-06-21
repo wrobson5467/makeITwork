7df305efd0eb9abe37697f701cf0c327336dee9e
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-06-21 16:40:23 -0700

    add style to nav bar and login
    Co-authored-by: Li Cheng 100156939+delacour124@users.noreply.github.com
    Co-authored-by: Alex Kolb akolb@clemson.edu
    Co-authored-by: Will Robson will.robson19@gmail.com
    Co-authored-by: Yang Song yangsong531@gmail.com
    Co-authored-by: Giovanni Flores-Lovo  83046950+gfloresl@users.noreply.github.com

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -4,15 +4,19 @@ import React from 'react';
 function Login(props){
     return (
       <div className='loginContainer'>
-        <form className='loginForm' method="POST" action='api/user/login'>
-          <label>Username</label>
-          <input type='text' name='username' required/><br></br>
-          <label>Password</label>
-          <input type='text' name='password' required/><br></br>
-          <button type='submit' id='loginButton'>Login</button>
-          <button type='button' id='forgotPWButton'>Forgot Password</button>
-        </form> 
-        <button onClick={useNavigate('/signup')}>Sign Up</button>
+        <div className='loginForm'>
+          <form method="POST" action='api/user/login'>
+            <label>Username</label>
+            <input type='text' name='username' required/><br></br>
+            <label>Password</label>
+            <input type='text' name='password' required/><br></br>
+            <button type='submit' id='loginButton'>Login</button>
+            <button type='button' id='forgotPWButton'>Forgot Password</button>
+          </form> 
+        </div>
+        <div className='signupButton'>
+          <button onClick={useNavigate('/signup')}>Sign Up</button>
+        </div>
       </div> 
     ) 
 }
