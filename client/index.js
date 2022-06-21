2671680ac267b91735b84ea7b9208256eec81f77
Yang Song <yangsong531@gmail.com>
2022-06-21 11:54:17 -0700

    complete back end signup
    Co-authored-by: Yang Song
    yangsong531@gmail.com
    Co-authored-by: Giovanni Flores-lovo 83046950+gfloresl@users.noreply.github.com

--- client/index.js
+++ client/index.js
@@ -3,8 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app'),
+  // <BrowserRouter>
+  <>
+     <App />
+  </>
+   
+  // {/* </BrowserRouter>, */}
+  ,document.getElementById('app'),
 );
